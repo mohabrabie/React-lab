5d7a2dcd0b31e80a4724d58fcf5ec3d39c25c988
mohamedalaa13 <mohmd46alaa@gmail.com>
2021-04-24 14:28:18 +0200

    Adding Fetch

---
+++ .history/src/components/PostList_20210424141126.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post'
+
+const PostList = (props) => {
+    return (
+        <div className="postList">
+            <Post title="first post" body="blablablablala"/>
+        </div>
+    )
+}
+
+export default PostList
