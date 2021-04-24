72572873349a7d31b3abebe746effd5f8cb53f23
mohabrabie <mohabhamed2020@gmail.com>
2021-04-24 13:55:13 +0200

    fixed issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header'
 import Form from './components/Form'
+
 function App() {
   return (
+    <div>
     <Header title="blog Application"/>
     <Form />
+    </div>
   );
 }
 
